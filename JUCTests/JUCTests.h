4081ab4166e78a1adfc70fbbc78f951cf4af96ba
Christopher Parola <cparola@octo.com>
2012-08-08 15:16:01 +0200

    Initial Commit

---
+++ JUCTests/JUCTests.h
@@ -0,0 +1,13 @@
+//
+//  JUCTests.h
+//  JUCTests
+//
+//  Created by Christopher PAROLA on 08/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JUCTests : SenTestCase
+
+@end
---
+++ JUCTests/JUCTests.m
@@ -0,0 +1,32 @@
+//
+//  JUCTests.m
+//  JUCTests
+//
+//  Created by Christopher PAROLA on 08/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "JUCTests.h"
+
+@implementation JUCTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JUCTests");
+}
+
+@end
